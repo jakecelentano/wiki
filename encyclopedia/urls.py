c78b90a23e189fcd45a5376dbffa9f70405a8bb6
jake <jakecelentano@gmail.com>
2020-10-14 09:57:45 -0400

    links & markdown

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("index/", views.index, name="index"),
+    path("index/?q=<str:query>", views.search, name="search"),
+    path("<str:name>", views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,36 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown2
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def entry(request, name):
+    entry = util.get_entry(name)
+    if entry is None:
+        return index(request) # not found page
+
+    return render(request, "encyclopedia/entry.html", {
+        "entrybody": markdown2.markdown(entry),
+        "name": name.capitalize(),
+
+    })
+
+def search(request, query):
+    result = util.get_entry(query)
+
+    if result is None:
+        return index(request) # list of results
+
+    return render(request, "encyclopedia/entry.html", {
+        "entries": util.list_entries()
+    })
+
+
+
 
